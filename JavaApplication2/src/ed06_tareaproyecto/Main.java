8afbef01a9eb00d4eb859f57ae36d6a2de21401d
alumno <alumno@10.0.2.15>
2017-05-29 16:45:22 +0200

    archivos subidos

---
+++ JavaApplication2/src/ed06_tareaproyecto/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed06_tareaproyecto;
+
+/**
+ *
+ * @author ED06
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("1245678M","Perico de los Palotes");
+        
+        empleado1.direccion="C/del Pez,6";
+        empleado1.setNumHijos(3);
+        imprimir_emp(empleado1);
+    }
+
+    public static void imprimir_emp(Empleado empleado1) {
+        System.out.println("DNI: "+empleado1.getDni());
+        System.out.println("Nombre: "+empleado1.getNombre());
+        System.out.println("Cargo: "+empleado1.getCargo());
+        System.out.println("Número de hijos: "+empleado1.getNumHijos());
+        System.out.println("Sueldo: "+empleado1.calcula_sueldo(1200));
+    }
+    
+}
